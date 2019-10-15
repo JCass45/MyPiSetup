af5aa1655c7600a006aefb9c518d7abbe1c9fcd8
Jack Cassidy <jackc@qualtrics.com>
2019-10-15 21:05:42 +0100

    add: Check for docker group

--- scripts/kick_start.sh
+++ scripts/kick_start.sh
@@ -15,10 +15,14 @@ sudo apt-get -y upgrade > /dev/null
 if [ ! $(command -v docker) ]; then
     echo "Installing Docker..."
     curl -sSL https://get.docker.com | sh
-    echo "Creating docker group for sudo-less Docker use"
-    sudo groupadd docker
-    sudo usermod -aG docker $USER
-    newgrp docker
+
+    if [ ! $(getent group docker) ]; then
+        echo "Creating docker group for sudo-less Docker use"
+        sudo groupadd docker
+        sudo usermod -aG docker $USER
+        newgrp docker
+    fi
+
     # Enable Docker on startup
     sudo systemctl enable docker
 else
@@ -26,14 +30,14 @@ else
 fi
 
 if [ ! $(command -v python) ] || [ ! -x $(command -v pip) ]; then
-    echo "Installing Python and pip"
+    echo "Installing Python and pip..."
     sudo apt-get -y install python python-pip > /dev/null
 else
     echo "Python and pip already installed"
 fi
 
 if [ ! $(command -v docker-compose) ]; then
-    echo "Installing Docker-Compose"
+    echo "Installing Docker-Compose..."
     sudo pip install docker-compose==1.23.2 > /dev/null
 else
     echo "Docker-Compose already installed"
